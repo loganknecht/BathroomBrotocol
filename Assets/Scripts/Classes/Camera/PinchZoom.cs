20e478d7e36c4b6890ef927988a24e420b2de4fb
Logan Knecht <loganknecht@gmail.com>
2014-09-14 19:33:18 -0700

    Updated Mark's Level

--- Assets/Scripts/Classes/Camera/PinchZoom.cs
+++ Assets/Scripts/Classes/Camera/PinchZoom.cs
@@ -6,13 +6,15 @@ public class PinchZoom : MonoBehaviour
 	public float zoomStepSize= 0.2f;
 
   // zoom-in and zoom-out limits
-	public float MaxZoom = 4.1f;
-  public float MinZoom = 1.0f;
+  public float minZoom = 1.0f;
+	public float maxZoom = 4.1f;
+  public float horizontalPanSpeedModifier = 0.1f;
+  public float verticalPanSpeedModifier = 0.1f;
 
 	private bool isPointerPressed;
 
 	private Vector2 prevDist = new Vector2(0,0);
-	private Vector2 curDist = new Vector2(0,0);
+	private Vector2 currentPinchDistance = new Vector2(0,0);
 	private Vector2 midPoint = new Vector2(0,0);
 	private Vector3 originalPos;
 
@@ -31,91 +33,97 @@ public class PinchZoom : MonoBehaviour
 
 	}
 	void Update () {
-		if(Input.GetMouseButtonDown(0)) {
-			isPointerPressed = true;
-    }
-		else if(Input.GetMouseButtonUp(0)) {
-			isPointerPressed = false;
-    }
 
-		if(Input.touchCount == 1
-        && Input.GetTouch(0).phase == TouchPhase.Began
-        && Input.GetTouch (0).tapCount == 1) {
-			return;
-		}
-
-		else if(Input.touchCount == 1
-            && Input.GetTouch(0).phase == TouchPhase.Moved) {
-			camera.transform.position += camera.transform.right * (-Input.GetTouch (0).deltaPosition.x) * 0.1f;
-			camera.transform.position += camera.transform.up * (-Input.GetTouch (0).deltaPosition.y) * 0.1f;
-		}
-		// These lines of code will pan/drag the object around untill the edge of the image
-		else if(isPointerPressed) {
-			camera.transform.position += camera.transform.right * (-Input.GetAxis("Mouse X")) * 0.1f;
-			camera.transform.position += camera.transform.up * (-Input.GetAxis("Mouse Y")) * 0.1f;
-		}
-
-		// On device (only) double tap image will be set at original position and scale
-		if(Input.touchCount == 1
-       && Input.GetTouch(0).phase == TouchPhase.Began
-       && Input.GetTouch (0).tapCount == 2) {
-			parentObject.transform.localScale = Vector3.one;
-			parentObject.transform.position = new Vector3 (originalPos.x * -1, originalPos.y * -1, originalPos.z * -1);
-			transform.position = originalPos;
-		}
-
-		//pc scrollWheel to zoom in and out.
-		if(Input.GetAxis ("Mouse ScrollWheel") > 0
-       && camera.orthographicSize > MinZoom) {
-			camera.orthographicSize -= zoomStepSize;
-		}
-		else if(Input.GetAxis ("Mouse ScrollWheel") < 0
-            && camera.orthographicSize < MaxZoom) {
-			camera.orthographicSize += zoomStepSize;
-		}
-		checkForMultiTouch();
 	}
 
+  private void PerformPinchZoomLogic() {
+    if(Input.GetMouseButtonDown(0)) {
+      isPointerPressed = true;
+    }
+    else if(Input.GetMouseButtonUp(0)) {
+      isPointerPressed = false;
+    }
+
+    // Returns if there's only been one tap detected and it just started
+    if(Input.touchCount == 1
+        && Input.GetTouch(0).phase == TouchPhase.Began
+        && Input.GetTouch (0).tapCount == 1) {
+      return;
+    }
+
+    else if(Input.touchCount == 1
+            && Input.GetTouch(0).phase == TouchPhase.Moved) {
+      camera.transform.position += camera.transform.right * (-Input.GetTouch (0).deltaPosition.x) * horizontalPanSpeedModifier;
+      camera.transform.position += camera.transform.up * (-Input.GetTouch (0).deltaPosition.y) * verticalPanSpeedModifier;
+    }
+    // These lines of code will pan/drag the object around untill the edge of the image
+    else if(isPointerPressed) {
+      camera.transform.position += camera.transform.right * (-Input.GetAxis("Mouse X")) * horizontalPanSpeedModifier;
+      camera.transform.position += camera.transform.up * (-Input.GetAxis("Mouse Y")) * verticalPanSpeedModifier;
+    }
+
+    // On device (only) double tap image will be set at original position and scale
+    if(Input.touchCount == 1
+       && Input.GetTouch(0).phase == TouchPhase.Began
+       && Input.GetTouch (0).tapCount == 2) {
+      parentObject.transform.localScale = Vector3.one;
+      parentObject.transform.position = new Vector3 (originalPos.x * -1, originalPos.y * -1, originalPos.z * -1);
+      transform.position = originalPos;
+    }
+
+    //pc scrollWheel to zoom in and out.
+    if(Input.GetAxis ("Mouse ScrollWheel") > 0
+       && camera.orthographicSize > minZoom) {
+      camera.orthographicSize -= zoomStepSize;
+    }
+    else if(Input.GetAxis ("Mouse ScrollWheel") < 0
+            && camera.orthographicSize < maxZoom) {
+      camera.orthographicSize += zoomStepSize;
+    }
+    CheckForMultiTouch();
+  }
+
 	// Following method check multi touch
-	private void checkForMultiTouch() {
+	private void CheckForMultiTouch() {
 		// These lines of code will take the distance between two touches and zoom in - zoom out at middle point between them
-		if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved)
-		{
+		if(Input.touchCount == 2
+       && Input.GetTouch(0).phase == TouchPhase.Moved
+       && Input.GetTouch(1).phase == TouchPhase.Moved) {
 			midPoint = new Vector2(((Input.GetTouch(0).position.x + Input.GetTouch(1).position.x)/2), ((Input.GetTouch(0).position.y + Input.GetTouch(1).position.y)/2));
 			midPoint = Camera.main.ScreenToWorldPoint(midPoint);
 
-			curDist = Input.GetTouch(0).position - Input.GetTouch(1).position;
+			currentPinchDistance = Input.GetTouch(0).position - Input.GetTouch(1).position;
 
 			//current distance between finger touches
 			prevDist = ((Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) - (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition));
 
 			//difference in previous locations using delta positions
-			float touchDelta = curDist.magnitude - prevDist.magnitude;
+			float touchDelta = currentPinchDistance.magnitude - prevDist.magnitude;
 
 			// device Zoom out
 			if(touchDelta<0) {
-				if(camera.orthographicSize < MaxZoom) {
+				if(camera.orthographicSize < maxZoom) {
 					camera.orthographicSize = camera.orthographicSize+zoomStepSize;
 				}
 			}
 
 			//device Zoom in
 			else if(touchDelta > 0) {
-				if(camera.orthographicSize > MinZoom) {
+				if(camera.orthographicSize > minZoom) {
 					camera.orthographicSize = camera.orthographicSize - zoomStepSize;
 				}
 			}
 
 			//mouse zoom out
 			if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-				if(camera.orthographicSize < MaxZoom) {
+				if(camera.orthographicSize < maxZoom) {
 					camera.orthographicSize = camera.orthographicSize + zoomStepSize;
 				}
 			}
 
 			//mouse zoom in
 			if (Input.GetAxis("Mouse ScrollWheel") > 0) {
-				if(camera.orthographicSize > MinZoom) {
+				if(camera.orthographicSize > minZoom) {
 					camera.orthographicSize = camera.orthographicSize + zoomStepSize;
 				}
 			}
